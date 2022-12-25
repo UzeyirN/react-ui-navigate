fadea2010cfa9131676859c198edd0177392d241
UzeyirN <7qzjlm4@code.edu.az>
2022-12-25 18:47:28 +0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+          
+      </Routes>
+      <Footer />
+
+
+
+    </BrowserRouter>
   );
 }
 
